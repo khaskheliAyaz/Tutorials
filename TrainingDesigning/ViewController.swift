4428d8d72a3a7ba34f65224ccc225729f3318096
ayaz akbar <ayazakbar24@gmail.com>
2018-05-01 11:22:47 +0500

    Initial Commit

---
+++ TrainingDesigning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrainingDesigning
+//
+//  Created by Ayaz Akbar on 5/1/18.
+//  Copyright © 2018 Ayaz Akbar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
